1b99a5e1172486e5a6d3474e0991ba4b428dc013
include-JoannaLopez <118097227+include-JoannaLopez@users.noreply.github.com>
2023-04-05 22:57:13 -0500

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -36,9 +36,15 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "ff5c2727752d3bd5e355bd0aa961bdf0";
-let city = "New York";
+let city = "San Antonio";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
