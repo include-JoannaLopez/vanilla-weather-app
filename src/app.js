fa0c2d76c1f9031b74c564fbc754a765a700efd1
include-JoannaLopez <118097227+include-JoannaLopez@users.noreply.github.com>
2023-04-11 19:05:53 -0500

    Removed HTML file & added display forecast  function
    Injected the HTML file to JS and created displayForecast function. Function called at the bottom of script. Created a grid with data injected from HTML. Created variable with days to test loop using fake data. Included parameter day to HTML string. Closed div concatenating to existing HTML.

--- src/app.js
+++ src/app.js
@@ -23,6 +23,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <img
+                    src="https://openweathermap.org/img/wn/02d@2x.png"
+                    alt=""
+                    width="42"
+                  />
+                  <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max">18° </span>
+                    <span class="weather-forecast-temperature-min">12° </span>
+                  </div>
+                </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -87,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("San Antonio");
+displayForecast();
